41c15f717a8aef7172f71ca92a42560a006401e0
Bruno-Gdos <Brunocosta1919@hotmail.com>
2022-07-06 16:59:12 -0300

    UPDATE 1.1

--- main.js
+++ main.js
@@ -1,68 +1,5 @@
-
-function play_ahuu() {
-    var audio = document.querySelector("#som_AHUURRAHBH");
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
-}
-
-
-function play_cavalo() {
-    var audio = document.querySelector("#som_cavalo");
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
-}
-
-
-function play_danca() {
-    var audio = document.querySelector("#som_danca_gatinho");
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
-}
-
-
-function play_ele_gosta() {
-    var audio = document.querySelector("#som_ele_gosta");
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
-}
-
-
-
-function play_iirra() {
-    var audio = document.querySelector("#som_iirraa");
-    if (audio.paused) {
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
-}
-
-
-
-function play_pare() {
-    var audio = document.querySelector("#som_pare");
+function play_som(idElementAudio) {
+    var audio = document.querySelector(idElementAudio);
     if (audio.paused) {
         audio.currentTime = 0
         audio.play();
@@ -73,51 +10,33 @@ function play_pare() {
     }
 }
 
-function play_atumalaca() {
-    var audio = document.querySelector("#som_atumalaca");
-    if (audio.paused) {
-        audio.currentTime = 0
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
-    }
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento);
+    const idAudio = `#som_${instrumento}`;
+    console.log(idAudio);
+    
+    tecla.onclick = function(){
+        play_som(idAudio);
+        
+    };
+
 }
 
-function play_aiaiai() {
-    var audio = document.querySelector("#som_aiaiai");
-    if (audio.paused) {
-        audio.currentTime = 0
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
+for (let indice = 0; indice < listaDeTeclas.length; indice++) {
+    const tecla = listaDeTeclas[indice];
+  
+    tecla.onkeydown = function (evento) {
+      if(evento.code === "Enter" || evento.code === "Space") {
+      tecla.classList.add('ativa');
+      }
     }
-}
-
-function play_tome() {
-    var audio = document.querySelector("#som_tome");
-    if (audio.paused) {
-        audio.currentTime = 0
-        audio.play();
-    }else{
-        audio.pause();
-        audio.currentTime = 0
-        audio.play()
+    tecla.onkeyup = function () {
+      tecla.classList.remove('ativa');
     }
-}
-
-
-
-document.querySelector(".tecla_danca_gatinho").onclick = play_danca;
-document.querySelector(".tecla_cavalo").onclick = play_cavalo;
-document.querySelector(".tecla_AHUURRAHBH").onclick = play_ahuu;
-document.querySelector(".tecla_danca_gatinho").onclick = play_danca;
-document.querySelector(".tecla_ele_gosta").onclick = play_ele_gosta;
-document.querySelector(".tecla_iirraa").onclick = play_iirra;
-document.querySelector(".tecla_pare").onclick = play_pare;
-document.querySelector(".tecla_atumalaca").onclick = play_atumalaca;
-document.querySelector(".tecla_aiaia").onclick = play_aiaiai;
-document.querySelector(".tecla_tome").onclick = play_tome;
+  }
